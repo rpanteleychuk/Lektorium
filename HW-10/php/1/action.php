8a41fb685fe44bda09ccc707eac986c9407a6883
rpanteleychuk <r.panteleychuk@gmail.com>
2019-02-12 11:00:05 +0200

    form processing done

---
+++ HW-10/php/1/action.php
@@ -0,0 +1,18 @@
+<?php
+$name =  ($_POST['name']);
+$surname =  ($_POST['surname']);
+$gender =  ($_POST['gender']);
+$age =  ($_POST['age']);
+$birthday =  ($_POST['birthday']);
+$email =  ($_POST['email']);
+$phone =  ($_POST['phone']);
+$password =  ($_POST['password']);
+$site =  ($_POST['site']);
+$hobby =  ($_POST['hobby']);
+$irritation =  ($_POST['irritation']);
+$comment =  ($_POST['comment']);
+$other =  ($_POST['other']);
+
+echo $name, $surname, $gender, $age, $birthday, $email, $phone, $phone, $password, $site, $hobby, $irritation, $comment, $other;
+
+
--- HW-10/php/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: prosst
- * Date: 08.02.2019
- * Time: 9:47
- */
